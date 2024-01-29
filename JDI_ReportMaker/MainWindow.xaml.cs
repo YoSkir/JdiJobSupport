d27045c31582a591c2b702dbb399133db579805d
YoSkir <137788381+YoSkir@users.noreply.github.com>
2024-01-29 22:33:31 +0800

    Initial
    初始化

---
+++ JDI_ReportMaker/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using MahApps.Metro.Controls;
+using Microsoft.Win32;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JDI_ReportMaker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void ButtonSelectExcel_Click(Object sender,RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Excel File|*.xls;*.xlsx;*.xlsm";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string filePath=openFileDialog.FileName;
+                textBoxFilePath.Text= filePath;
+            }
+        }
+    }
+}
